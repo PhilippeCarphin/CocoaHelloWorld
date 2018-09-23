6c34b6039dddb55a2c0b7ca8df87804f08c23dee
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-09-23 14:55:09 -0400

    Initial Commit

---
+++ CocoaHelloWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  CocoaHelloWorld
+//
+//  Created by Philippe Carphin on 2018-09-23.
+//  Copyright © 2018 Philippe Carphin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
