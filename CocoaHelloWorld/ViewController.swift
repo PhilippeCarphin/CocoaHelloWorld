306fac880f40996e5b1d14dfa1751eb404bb77f0
Philippe Carphin <philippe.carphin@polymtl.ca>
2018-09-23 15:27:54 -0400

    Followed the first steps from the tutoral
    https://www.raywenderlich.com/731-macos-development-for-beginners-part-1

--- CocoaHelloWorld/ViewController.swift
+++ CocoaHelloWorld/ViewController.swift
@@ -10,6 +10,17 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var nameField: NSTextField!
+    
+    @IBOutlet weak var helloLabel: NSTextField!
+    @IBAction func sayButtonClicked(_ sender: Any) {
+        var name = nameField.stringValue
+        if name.isEmpty {
+            name = "World"
+        }
+        let greeting = "Hello \(name)!"
+        helloLabel.stringValue = greeting
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
